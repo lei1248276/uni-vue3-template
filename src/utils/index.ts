adf220ec2c5c1fadc8e9d80709936f75958e5d4c
Jaye <jaye1248276@gmail.com>
2023-11-13 13:56:20 +0800

    chore: 添加本地图片路径转换方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,18 @@
 export function validatePhone(phone: string) {
   return /^(?:(?:\+|00)86)?1\d{10}$/.test(phone + '')
 }
+
+/**
+ * @description 转换静态图片路径(`/src/static/img/${iconPath}`)
+ * @param iconPath 图片地址(需要带后缀)
+ */
+export function transImg(iconPath: string) {
+  // #ifndef APP-PLUS
+  return `/static/img/${iconPath}`
+  // #endif
+
+  // #ifdef H5
+  // eslint-disable-next-line
+  return new URL(`/src/static/img/${iconPath}`, import.meta.url).href
+  // #endif
+}
