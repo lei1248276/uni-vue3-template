8e83118f31b51579cf220ed7a965697762c15794
Jaye <jaye1248276@gmail.com>
2024-10-22 17:04:32 +0800

    feat(component): 新增JIcon全局组件

---
+++ src/components/JIcon/JIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <span
+    class="iconfont"
+    :class="type"
+  >
+    <slot v-if="$slots.default" />
+  </span>
+</template>
+
+// #ifdef MP-WEIXIN
+<script lang="ts">
+export default {
+  options: {
+    virtualHost: true
+  }
+}
+</script>
+// #endif
+
+<script setup lang="ts">
+defineProps<{
+  type: `icon-${string}`
+}>()
+</script>
