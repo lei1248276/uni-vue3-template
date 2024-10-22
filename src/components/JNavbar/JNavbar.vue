c8d2c7c0b8f1db773834e3c398e1df186626bbc7
Jaye <jaye1248276@gmail.com>
2024-10-22 17:04:52 +0800

    feat(component): 新增JNavbar全局组件

--- src/components/JNavbar/JNavbar.vue
+++ src/components/JNavbar/JNavbar.vue
@@ -1,45 +1,88 @@
 <template>
-  <view :style="{paddingTop: statusBarHeight + 'px', color: color || '#333'}">
+  <div
+    class="relative z-[999]"
+    :class="{'before:block before:h-[44px] before:w-full': fixed,}"
+    :style="{paddingTop: statusBarHeight + 'px', color: color}"
+  >
+    <!-- #ifdef MP-WEIXIN -->
     <view
-      v-if="fixed"
-      class="w-full h-[44px]"
-    />
-
-    <view
-      class="box-content w-full h-[44px] flex items-center justify-between"
-      :class="{'z-50 fixed !top-0 left-0 right-0': fixed}"
+      class="flex h-[44px] w-full items-center justify-between"
+      :class="{'fixed inset-x-0 top-0 z-50': fixed}"
       :style="{
-        backgroundColor: backgroundColor || 'transparent',
-        top: statusBarHeight + 'px',
+        backgroundColor: backgroundColor,
         paddingTop: fixed ? statusBarHeight + 'px' : 0
       }"
     >
-      <view class="flex-[2]">
-        <slot
-          v-if="$slots.left"
-          name="left"
-        />
-        <image
-          v-else-if="leftArrow || leftIcon"
-          :src="leftIcon || iconBack"
-          class="w-[40rpx] h-[40rpx] ml-[20rpx] btn-zoom"
-          @tap.stop="onBack"
-        />
-      </view>
+      <!-- #endif -->
 
-      <view class="flex-[6] text-[32rpx] text-center">
-        <slot v-if="$slots.default" />
-        <text v-else>{{ title || '' }}</text>
-      </view>
+      <!-- #ifndef MP -->
+      <component
+        :is="isCoverView ? 'cover-view' : 'div'"
+        class="flex min-h-[44px] w-full items-center justify-between"
+        :class="{'fixed inset-x-0 top-0 z-50': fixed}"
+        :style="{
+          backgroundColor: backgroundColor,
+          paddingTop: fixed ? statusBarHeight + 'px' : 0
+        }"
+      >
+        <!-- #endif -->
 
-      <view class="flex-[2]">
-        <slot
-          v-if="$slots.right"
-          name="right"
-        />
-      </view>
+        <div class="flex h-[44px] flex-[2] items-center">
+          <slot
+            v-if="$slots.left"
+            name="left"
+          />
+          <JIcon
+            v-else-if="leftArrow &&leftIcon"
+            :type="leftIcon"
+            :class="arrowClass"
+            class="btn-zoom ml-[20rpx] text-[40rpx]"
+            @tap.stop="useNavigate(1)"
+          />
+          <cover-image
+            v-else-if="leftArrow && isCoverView"
+            :src="leftImg || iconBack"
+            class="btn-zoom ml-[20rpx] size-[40rpx]"
+            @tap="useNavigate(1)"
+          />
+          <image
+            v-else-if="leftArrow"
+            :src="leftImg || iconBack"
+            :class="arrowClass"
+            class="btn-zoom ml-[20rpx] size-[40rpx]"
+            @tap.stop="useNavigate(1)"
+          />
+        </div>
+
+        <div class="flex h-[44px] flex-[6] items-center justify-center text-center react-text-[32rpx]">
+          <slot v-if="$slots.default" />
+          <cover-view
+            v-else-if="isCoverView"
+            :class="titleClass"
+          >
+            {{ title }}
+          </cover-view>
+          <span
+            v-else
+            :class="titleClass"
+          >{{ title }}</span>
+        </div>
+
+        <div class="h-[44px] flex-[2]">
+          <slot
+            v-if="$slots.right"
+            name="right"
+          />
+        </div>
+
+      <!-- #ifndef MP -->
+      </component>
+      <!-- #endif -->
+
+    <!-- #ifdef MP-WEIXIN -->
     </view>
-  </view>
+    <!-- #endif -->
+  </div>
 </template>
 
 // #ifdef MP-WEIXIN
@@ -53,22 +96,32 @@ export default {
 // #endif
 
 <script setup lang="ts">
-import iconBack from './icon-back.png'
+import iconBack from './img/back.png'
 
-interface NavBarProps {
+withDefaults(defineProps<{
   title?: string
+  titleClass?: string
   leftArrow?: boolean
-  leftIcon?: string
+  arrowClass?: string
+  leftIcon?: `icon-${string}`
+  leftImg?: string
   color?: string
   backgroundColor?: string
   fixed?: boolean
-}
+  isCoverView?: boolean
+}>(), {
+  title: undefined,
+  titleClass: undefined,
+  leftArrow: false,
+  arrowClass: undefined,
+  leftIcon: undefined,
+  leftImg: undefined,
+  color: '#333',
+  backgroundColor: '#fff',
+  fixed: false,
+  isCoverView: false
+})
 
-defineProps<NavBarProps>()
+const statusBarHeight = useSystemInfo().statusBarHeight
 
-const statusBarHeight = useStatusBarHeight()
-
-function onBack() {
-  uni.navigateBack()
-}
 </script>
