738b525bca6102fb4665fc2d2611945b7f700348
Jaye <jaye1248276@gmail.com>
2024-10-23 12:58:48 +0800

    refactor: 新增vue模板全局变量

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 
+declare module 'vue' {
+  interface ComponentCustomProperties {
+      // eslint-disable-next-line no-undef
+      env: ImportMetaEnv
+      // eslint-disable-next-line no-undef
+      console: Console
+  }
+}
+
 export function createApp() {
   const app = createSSRApp(App)
   const pinia = createPinia()
+
+  app.config.globalProperties.env = import.meta.env
+  app.config.globalProperties.console = console
 
   app.use(pinia)
 
