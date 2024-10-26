acf6de59073387a1540e906579849fc94b794e47
Jaye <jaye1248276@gmail.com>
2024-10-26 20:32:12 +0800

    perf(component): JIcon组件支持图片图标

--- src/components/JIcon/JIcon.vue
+++ src/components/JIcon/JIcon.vue
@@ -1,24 +1,27 @@
 <template>
   <span
-    class="iconfont"
-    :class="type"
+    class="iconfont bg-full inline-block size-[32rpx] text-[32rpx]"
+    :class="icon?.startsWith('icon-') ? icon : undefined"
+    :style="{ 'background-image': !icon || icon.startsWith('icon-') ? undefined : `url(${icon})` }"
   >
     <slot v-if="$slots.default" />
   </span>
 </template>
 
-// #ifdef MP-WEIXIN
 <script lang="ts">
 export default {
+  name: 'JIcon',
   options: {
+    // #ifdef MP-WEIXIN
     virtualHost: true
+    // #endif
   }
 }
 </script>
-// #endif
 
 <script setup lang="ts">
 defineProps<{
-  type: `icon-${string}`
+  /** 可以为字体图标 `icon-${string}` 或图片路径 */
+  icon: `icon-${string}` | string
 }>()
 </script>
