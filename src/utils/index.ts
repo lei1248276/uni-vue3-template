3ff676c37e7d4ed9a1162960d8e87903af56b3f0
Jaye <jaye1248276@gmail.com>
2024-10-30 17:03:20 +0800

    perf(util): 优化remove方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,39 @@
+/** console.log 别名 */
+export const log = console.log
+
 /** Object.assign别名 */
 export const extend: typeof Object.assign = Object.assign
 
-/** 删除数组元素 */
-export const remove = <T>(arr: T[], el: T): void => {
-  const i = arr.indexOf(el)
+/**
+ * 删除数组元素
+ * @example
+ * ```ts
+ * remove([1, 2, 3], 2) // [1, 3]
+ * remove([{v: 1}, {v: 2}, {v: 3}], (v) => v.v === 2) // [{v: 1}, {v: 3}]
+ * ```
+ * */
+export const remove = <T>(
+  arr: T[],
+  el: T | ((value: T, index: number, array: T[]) => boolean)
+): boolean => {
+  const i = typeof el === 'function'
+    ? arr.findIndex(el as (value: T, index: number, array: T[]) => boolean)
+    : arr.indexOf(el as T)
+
   if (i > -1) {
     arr.splice(i, 1)
+    return true
   }
+  return false
 }
 
-/** 数组切片 */
+/**
+ * 数组切片
+ * @example
+ * ```ts
+ * chunk([1, 2, 3, 4, 5], 2) // [[1, 2], [3, 4], [5]]
+ * ```
+ * */
 export const chunk = <T>(arr: T[], size: number): T[][] => {
   const res: T[][] = []
   for (let i = 0; i < arr.length; i += size) {
